935288682202928ebe6c41f21cbc9d5ef6fe19a7
NerellaSumanth <sumanthna4@gmail.com>
2020-11-15 11:04:16 +0530

    Fix start date error

--- application/controllers/Trainer_Add_Courses/Training_section.php
+++ application/controllers/Trainer_Add_Courses/Training_section.php
@@ -32,8 +32,8 @@ class Training_section extends UserController {
                 'sort_order' => $sort_order_array[$key],
                 'created_by' => $this->global['trainerId'],
                 'created_date' => date('Y-m-d H:i:s'),
-                'modified_by' => '',
-                'modified_date' => '',
+                'modified_by' => '0',
+                'modified_date' => '0000-00-00',
             ); 
         }
 
@@ -198,8 +198,8 @@ class Training_section extends UserController {
             'sort_order'=>$this->input->post('sort_order'),
             'created_by' => $this->global['trainerId'],
             'created_date' => date('Y-m-d H:i:s'),
-            'modified_by' => '',
-            'modified_date' => '',
+            'modified_by' => '0',
+            'modified_date' => '0000-00-00',
 	        
 	    );
 	  
