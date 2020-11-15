226236302d84d9cc060bc47a8105078a6584651c
Hignosis <74319082+hignosisgit@users.noreply.github.com>
2020-11-15 16:36:19 +0530

    Update add_training_script.php

--- application/views/trainer-includes/add_training_script.php
+++ application/views/trainer-includes/add_training_script.php
@@ -984,6 +984,8 @@ $('.add_concept').bind('click', function(e){
 
 					$('#section_progress_'+id).removeClass('hide');
 					$('#section_details_form_button_'+id).addClass('hide');
+					
+					console.log(fd);
 
 					$.ajax({
 
