0a4d6ee28b5b211c164f3d4de8532834fa1a436a
Kshirod123 <kshirodkumar81@gmail.com>
2020-11-17 22:09:09 +0530

    commision issue fixing

--- admin/application/models/commission/Commission_model.php
+++ admin/application/models/commission/Commission_model.php
@@ -18,15 +18,11 @@ class Commission_model extends CI_Model {
 	    $this->db->query("UPDATE  commission set platform_commission = '" . $data['platform_commission'] . "', commission_start_date = '" . $data['commission_start_date'] . "',status='1'  where commission_id= '".$commission_id."' ");
 	    
         $this->db->trans_complete();
-        
-        return $user_id;
     }
     
 
     function getCommissionAll($customer_id) {
-         $query = $this->db->query("SELECT *,c.status as comm_status FROM `commission` as c, `customer` as ct WHERE c.trainer_id = ct.customer_id and c.trainer_id='".$customer_id."' order by commission_start_date desc");
-
-     //   $query = $this->db->query("SELECT * FROM `commission` where customer_id= '".$customer_id."'");
+        $query = $this->db->query("SELECT *,c.status as comm_status FROM `commission` as c, `customer` as ct WHERE c.trainer_id = ct.customer_id and c.trainer_id='".$customer_id."' order by commission_id desc");
         $result = $query->result();
         return $result;
     }
